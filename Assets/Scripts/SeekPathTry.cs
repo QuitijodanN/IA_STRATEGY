713b7e14958e74e0667c1dedab928110780df7ad
UJIjms <al405003@uji.es>
2024-12-03 19:16:42 +0100

    TryScript

---
+++ Assets/Scripts/SeekPathTry.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class SeekPathTry : MonoBehaviour
+{
+    [SerializeField] Grid grid;
+
+    Node[] path;
+    int targetIndex = 0;
+
+    public void Try()
+    {
+        PathRequestManager.RequestPath(grid.grid[1,2], grid.grid[3, 7], OnPathFound);
+    }
+
+    public void OnPathFound(Node[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        if (path.Length > 0)
+        {
+            Node currentNode = path[0];
+            while (targetIndex < path.Length)
+            {
+                currentNode = path[targetIndex];
+                Debug.Log(currentNode.gridY + "y /" + currentNode.gridX + "x");
+                targetIndex++;
+
+                yield return null;
+            }
+        }
+    }
+}
